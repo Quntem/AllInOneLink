45c50959d9a216ff1e7d97a9fba6510feffecac9
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-01-17 23:51:40 +0000

    Added basic creating of pages, as well as adding of links

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 var express = require("express")
 var app = express()
+var fs = require("fs")
+var path = require("path")
+app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, '/src'));
 var port = 5500
+var currentfile = ""
 
 app.use("/", express.static("src"))
+
+app.get('/:name', (req, res) => {
+    res.render('useraiolpage', { username: req.params.name })
+})
+
+app.post('/api/page/create/', (req, res) => {
+    fs.writeFile("linkindex/" + req.query.pagename + ".json", JSON.stringify({
+        "name": req.query.pagename,
+        "userid": req.query.userid,
+        "links": [
+
+        ]
+    }), err => {
+        if (!err) {
+            res.status(200).send("Success")
+        }
+    })
+})
+
+app.post('/api/page/links/add/', (req, res) => {
+    fs.readFile("linkindex/" + req.query.pagename + ".json", 'utf8', (err, data) => {
+        if (!err) {
+            currentfile = JSON.parse(data)
+            currentfile.links.push({
+                "id": new Date().toString(),
+                "name": req.query.name,
+                "url": req.query.url
+            })
+            fs.writeFile("linkindex/" + req.query.pagename + ".json", JSON.stringify(currentfile), err => {
+                if (!err) {
+                    res.status(200).send("Success")
+                }
+            })
+        }
+    });
+})
 
 app.listen(5500, () => {
     console.log("Listening on " + port)
