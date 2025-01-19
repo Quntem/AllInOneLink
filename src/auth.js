df3ca19a1532a0d7c9237d0b61b0473fe994473e
Oscar May <67429870+OscarMayReal@users.noreply.github.com>
2025-01-19 12:43:56 +0000

    users now see their own page

--- src/auth.js
+++ src/auth.js
@@ -5,6 +5,13 @@ window.addEventListener('load', async function () {
       const userButtonDiv = document.getElementById('user-button')
 
       Clerk.mountUserButton(userButtonDiv)
+
+        if (Clerk.user.publicMetadata.mainpage == undefined) {
+            window.location.assign("/setupflow/")
+        } else {
+            currentpage = Clerk.user.publicMetadata.mainpage
+            pagefunction()
+        }
     } else {
       Clerk.redirectToSignIn()
     }
--- src/dashboard-links.js
+++ src/dashboard-links.js
@@ -1,15 +1,17 @@
-fetch("api/page/" + currentpage + "/links/list")
-.then(res => res.json())
-.then(res => {
-    $("#linkListArea").html("")
-    res.forEach(link => {
-        $("#linkListArea").append(`
-            <div class="Link">
-                <label for="LinkInput">Url</label>
-                <input type="url" name="LinkInput" placeholder="Link URL" value="` + link.url + `">
-                <label for="LinkNameInput">Name</label>
-                <input type="url" name="LinkNameInput" placeholder="Link Name" value="` + link.name + `">
-            </div>
-        `)
-    });
-})
+var pagefunction = function() {
+    fetch("api/page/" + currentpage + "/links/list")
+        .then(res => res.json())
+        .then(res => {
+            $("#linkListArea").html("")
+            res.forEach(link => {
+                $("#linkListArea").append(`
+                    <div class="Link">
+                        <label for="LinkInput">Url</label>
+                        <input type="url" name="LinkInput" placeholder="Link URL" value="` + link.url + `">
+                        <label for="LinkNameInput">Name</label>
+                        <input type="url" name="LinkNameInput" placeholder="Link Name" value="` + link.name + `">
+                    </div>
+                `)
+            });
+        })
+}
